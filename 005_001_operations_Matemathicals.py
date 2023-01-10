1b1872e80f48355a642edbca71cca38006146273
Cesarpipe31 <cesarpipe31ingenieria@gmail.com>
2023-01-10 15:07:59 -0500

    Publicando

---
+++ 005_001_operations_Matemathicals.py
@@ -0,0 +1,14 @@
+"""
+    Write A Program That Performs All Arithmetic Operations On Two Variables.
+
+"""
+
+firstNumber = int(input("Enter first number: "))
+secondNumber = int(input("Enter second number: "))
+
+print("\n")
+print("Addtion of two number : " + str(firstNumber + secondNumber))
+print("Subtraction of two number : " + str(firstNumber - secondNumber))
+print("Multiplication of two number : " + str(firstNumber*secondNumber))
+print("Division of two number : " + str(firstNumber / secondNumber))
+print("Module of two numbers : " + str(firstNumber%secondNumber))
