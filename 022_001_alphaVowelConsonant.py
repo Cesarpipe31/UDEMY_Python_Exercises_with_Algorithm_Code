b2245f92734d9dd8dadb39b8d6988c1f9fb0dc19
Cesarpipe31 <cesarpipe31ingenieria@gmail.com>
2023-01-14 10:05:51 -0500

    Publicando

---
+++ 022_001_alphaVowelConsonant.py
@@ -0,0 +1,18 @@
+"""
+Write A Python Program To Check Alpha Character, Whether Vowel Or Consonant
+"""
+
+oneCharacter = char(input("Enter your character: "))
+
+if (oneCharacter == "a" or oneCharacter == "e" or oneCharacter == "i" or oneCharacter == "o" or oneCharacter == "u"):
+    print("Character is vowel: " +oneCharacter)
+else:
+    print("Character is consonant: " +oneCharacter)
+
+
+
+
+"""
+Write a Python program to check alpha character, whether vowel or consonant. 
+And also display “it is number”, if user give any number.
+"""
